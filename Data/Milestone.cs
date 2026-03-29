a124ea894727238d17b6eed75b3d5dca970a08dd
Chase <itschase88@gmail.com>
2026-03-29 17:09:51 -0500

    Feat: Built out database/class schema

---
+++ Data/Milestone.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portfolio.Data;
+
+public class Milestone
+{
+    [Key]
+    public int ID { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+
+    [ForeignKey("MilestoneType")]
+    public int MilestoneTypeID { get; set; }
+    public MilestoneType milestoneType { get; set; } = null!;
+    public string? Description { get; set; }
+
+    public int YearStart { get; set; }
+    public int? YearEnd { get; set; }
+}
---
+++ Data/MilestoneType.cs
@@ -0,0 +1,15 @@
+﻿using MudBlazor;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data;
+
+public class MilestoneType
+{
+    [Key]
+    public int ID { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public string Icon { get; set; } = Icons.Material.Filled.QuestionMark;
+
+    public List<Milestone> Milestones { get; set; } = [];
+}
